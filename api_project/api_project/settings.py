87976020f510a37f1393a664b5c2eb5dee0d507a
akshayrgaikwad <124763488+akshayrgaikwad@users.noreply.github.com>
2023-10-26 15:02:56 +0530

    Update settings.py
    sqlite database added so that anybody can access the project.

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'api_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mssql' ,
-        'NAME': 'api',
-        'USER':'' ,
-        'PASSWORD': '',
-        'HOST' : 'DESKTOP-2USMQTR\SQLEXPRESS'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
